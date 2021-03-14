009d2dcf8b8b370c38fb4f854dd1a24695476d27
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2021-03-14 10:31:35 -0500

    changed gradient values

--- main.cpp
+++ main.cpp
@@ -8,8 +8,28 @@ int main(){
     PerlinNoise::generateNewPermutation();
     for(int i = 0; i < HEIGHT; i++){
         for(int j = 0; j < WIDTH; j++){
-            double noiseVal = PerlinNoise::perlin((double)i/HEIGHT+1,(double)j/WIDTH+1,1);
-
+            double noiseVal = PerlinNoise::perlin((double)i/HEIGHT+1,(double)j/WIDTH+1,0);
+            if(noiseVal < .1)
+                std::cout << ' ';
+            else if(noiseVal < .2)
+                std::cout << '.';
+            else if(noiseVal < .3)
+                std::cout << ':';
+            else if(noiseVal < .4)
+                std::cout << ';';
+            else if(noiseVal < .5)
+                std::cout << '+';
+            else if(noiseVal < .6)
+                std::cout << '=';
+            else if(noiseVal < .7)
+                std::cout << 'x';
+            else if(noiseVal < .8)
+                std::cout << 'X';
+            else if(noiseVal < .9)
+                std::cout << '$';
+            else
+                std::cout << '&';
+/*
             if(noiseVal > .75)
                 std::cout << "░";
             else if(noiseVal > .5)
@@ -18,6 +38,7 @@ int main(){
                 std::cout << "▓";
             else
                 std::cout << "█";
+                */
         }
         std::cout << std::endl;
     }
